f04c9d5ddb06099c841447733927e4c33fb19e75
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2016-02-22 17:00:48 -0500

    Paso 1 DTOs y configuración Jersey

---
+++ bookstore-web/src/main/java/co/edu/uniandes/csw/bookstore/dtos/BookDTO.java
@@ -0,0 +1,66 @@
+package co.edu.uniandes.csw.bookstore.dtos;
+
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @generated
+ */
+@XmlRootElement
+public class BookDTO {
+
+    private Long id;
+    private String name;
+    private String isbn;
+    private String image;
+    private Date publishDate;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+}
---
+++ bookstore-web/src/main/java/co/edu/uniandes/csw/bookstore/services/RestConfig.java
@@ -0,0 +1,13 @@
+package co.edu.uniandes.csw.bookstore.services;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("api")
+public class RestConfig extends ResourceConfig {
+
+    public RestConfig() {
+        packages("co.edu.uniandes.csw.bookbasico.services");
+        packages("co.edu.uniandes.csw.auth.provider");
+    }
+}
