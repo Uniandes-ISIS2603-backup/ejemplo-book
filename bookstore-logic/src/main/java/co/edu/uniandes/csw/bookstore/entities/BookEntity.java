d5612fbcabe780dfd05627583805c6cbf0c38f36
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2016-02-24 13:56:22 -0500

    Entidad y persistencia

---
+++ bookstore-logic/src/main/java/co/edu/uniandes/csw/bookstore/entities/BookEntity.java
@@ -0,0 +1,74 @@
+package co.edu.uniandes.csw.bookstore.entities;
+
+import co.edu.uniandes.csw.crud.spi.entity.BaseEntity;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class BookEntity extends BaseEntity implements Serializable {
+
+    private String isbn;
+    private String image;
+    @Temporal(TemporalType.DATE)
+    private Date publishDate;
+    private String description;
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * @return the publishDate
+     */
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    /**
+     * @param publishDate the publishDate to set
+     */
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- bookstore-web/src/main/java/co/edu/uniandes/csw/bookstore/services/BookService.java
+++ bookstore-web/src/main/java/co/edu/uniandes/csw/bookstore/services/BookService.java
@@ -1,8 +1,12 @@
 package co.edu.uniandes.csw.bookstore.services;
 
 import co.edu.uniandes.csw.bookstore.dtos.BookDTO;
+import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,9 +18,30 @@ import javax.ws.rs.core.MediaType;
 public class BookService {
 
     @GET
+    public List<BookDTO> getBook() {
+        return null; //Implementar
+    }
+
+    @GET
     @Path("{id: \\d+}")
     public BookDTO getBook(@PathParam("id") Long id) {
-        // Implementación
-        return null;
+        return null; //Implementar
+    }
+
+    @POST
+    public BookDTO createBook(BookDTO dto) {
+        return null; //Implementar
+    }
+
+    @PUT
+    @Path("{id: \\d+}")
+    public BookDTO updateBook(@PathParam("id") Long id, BookDTO dto) {
+        return null; //Implementar
+    }
+
+    @DELETE
+    @Path("{id: \\d+}")
+    public void deleteBook(@PathParam("id") Long id) {
+
     }
 }
