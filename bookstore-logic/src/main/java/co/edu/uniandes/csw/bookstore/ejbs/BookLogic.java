0d07ea3e4ce5685b50419d6990bccb9dc23f9114
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2016-03-08 13:32:17 -0500

    Implementación Logger

--- bookstore-logic/src/main/java/co/edu/uniandes/csw/bookstore/ejbs/BookLogic.java
+++ bookstore-logic/src/main/java/co/edu/uniandes/csw/bookstore/ejbs/BookLogic.java
@@ -5,43 +5,59 @@ import co.edu.uniandes.csw.bookstore.entities.BookEntity;
 import co.edu.uniandes.csw.bookstore.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.bookstore.persistence.BookPersistence;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 @Stateless
 public class BookLogic implements IBookLogic {
 
+    private static final Logger logger = Logger.getLogger(BookLogic.class.getName());
+
     @Inject
     private BookPersistence persistence;
 
     @Override
     public List<BookEntity> getBooks() {
-        return persistence.findAll();
+        logger.info("Inicia proceso de consultar todos los libros");
+        List<BookEntity> books = persistence.findAll();
+        logger.info("Termina proceso de consultar todos los libros");
+        return books;
     }
 
     @Override
     public BookEntity getBook(Long id) throws BusinessLogicException {
+        logger.log(Level.INFO, "Inicia proceso de consultar libro con id={0}", id);
         BookEntity book = persistence.find(id);
         if (book == null) {
-            throw new BusinessLogicException("There's no book with requested id");
+            logger.log(Level.SEVERE, "El libro con el id {0} no existe", id);
+            throw new BusinessLogicException("El libro solicitado no existe");
         }
+        logger.log(Level.INFO, "Termina proceso de consultar libro con id={0}", id);
         return book;
     }
 
     @Override
     public BookEntity createBook(BookEntity entity) {
+        logger.info("Inicia proceso de creación de libro");
         persistence.create(entity);
+        logger.info("Termina proceso de creación de libro");
         return entity;
     }
 
     @Override
     public BookEntity updateBook(BookEntity entity) {
-        BookEntity newEntity = entity;
-        return persistence.update(newEntity);
+        logger.log(Level.INFO, "Inicia proceso de actualizar libro con id={0}", entity.getId());
+        BookEntity newEntity = persistence.update(entity);
+        logger.log(Level.INFO, "Termina proceso de actualizar libro con id={0}", entity.getId());
+        return newEntity;
     }
 
     @Override
     public void deleteBook(Long id) {
+        logger.log(Level.INFO, "Inicia proceso de borrar libro con id={0}", id);
         persistence.delete(id);
+        logger.log(Level.INFO, "Termina proceso de borrar libro con id={0}", id);
     }
 }
--- bookstore-web/src/main/java/co/edu/uniandes/csw/bookstore/resources/BookResource.java
+++ bookstore-web/src/main/java/co/edu/uniandes/csw/bookstore/resources/BookResource.java
@@ -5,6 +5,8 @@ import co.edu.uniandes.csw.bookstore.converters.BookConverter;
 import co.edu.uniandes.csw.bookstore.dtos.BookDTO;
 import co.edu.uniandes.csw.bookstore.exceptions.BusinessLogicException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -21,28 +23,34 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
+    private static final Logger logger = Logger.getLogger(BookResource.class.getName());
+
     @Inject
     private IBookLogic bookLogic;
 
     @GET
     public List<BookDTO> getBook() {
+        logger.info("Se ejecuta método getBooks");
         return BookConverter.listEntity2DTO(bookLogic.getBooks());
     }
 
     @GET
     @Path("{id: \\d+}")
     public BookDTO getBook(@PathParam("id") Long id) throws BusinessLogicException {
+        logger.log(Level.INFO, "Se ejecuta método getBook con id={0}", id);
         return BookConverter.basicEntity2DTO(bookLogic.getBook(id));
     }
 
     @POST
     public BookDTO createBook(BookDTO dto) {
+        logger.info("Se ejecuta método createBook");
         return BookConverter.basicEntity2DTO(bookLogic.createBook(BookConverter.basicDTO2Entity(dto)));
     }
 
     @PUT
     @Path("{id: \\d+}")
     public BookDTO updateBook(@PathParam("id") Long id, BookDTO dto) {
+        logger.log(Level.INFO, "Se ejecuta método updateBook con id={0}", id);
         dto.setId(id);
         return BookConverter.basicEntity2DTO(bookLogic.updateBook(BookConverter.basicDTO2Entity(dto)));
     }
@@ -50,6 +58,7 @@ public class BookResource {
     @DELETE
     @Path("{id: \\d+}")
     public void deleteBook(@PathParam("id") Long id) {
+        logger.log(Level.INFO, "Se ejecuta método deleteBook con id={0}", id);
         bookLogic.deleteBook(id);
     }
 }
