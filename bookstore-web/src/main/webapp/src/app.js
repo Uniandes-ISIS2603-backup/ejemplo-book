1f73a60023d5e229749c1d1431142071ed0228d3
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2016-03-09 17:05:39 -0500

    Se quita las otras entidades

--- bookstore-web/src/main/webapp/src/app.js
+++ bookstore-web/src/main/webapp/src/app.js
@@ -3,13 +3,6 @@
     var mod = ng.module("mainApp", [
         "ui.router",
         "bookModule",
-        "editorialModule",
-        "authorModule",
-        "reviewModule",
-//        "bookMock",
-//        "editorialMock",
-//        "authorMock",
-//        "reviewMock",
         "ngMessages"
     ]);
 
@@ -25,24 +18,6 @@
                         controller: "bookCtrl",
                         controllerAs: "ctrl",
                         templateUrl: "src/modules/book/book.tpl.html"
-                    })
-                    .state('editorial', {
-                        url: '/editorial',
-                        controller: "editorialCtrl",
-                        controllerAs: "ctrl",
-                        templateUrl: "src/modules/editorial/editorial.tpl.html"
-                    })
-                    .state('author', {
-                        url: '/author',
-                        controller: "authorCtrl",
-                        controllerAs: "ctrl",
-                        templateUrl: "src/modules/author/author.tpl.html"
-                    })
-                    .state('review', {
-                        url: '/review',
-                        controller: "reviewCtrl",
-                        controllerAs: "ctrl",
-                        templateUrl: "src/modules/review/review.tpl.html"
                     });
         }]);
 })(window.angular);
